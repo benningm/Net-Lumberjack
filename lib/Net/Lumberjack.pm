c67dcdbbcc9e56c6340b004864022f52ff0613a8
Markus Benning <ich@markusbenning.de>
2016-11-13 11:50:13 +0100

    Add a file for namespace Net::Lumberjack

---
+++ lib/Net/Lumberjack.pm
@@ -0,0 +1,20 @@
+package Net::Lumberjack;
+
+use strict;
+use warnings;
+
+# VERSION
+# ABSTRACT: client/server protocol implementation for lumberjack
+
+=head1 CLIENT
+
+For implementing a lumberjack protocol client see L<Net::Lumberjack::Client>.
+
+=head2 SERVER
+
+For implementing a lumberjack protocol server see L<Net::Lumberjack::Reader>.
+
+=cut
+
+1;
+
